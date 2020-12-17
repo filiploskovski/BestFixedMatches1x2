22b69cdc05d01ae9c89fe2314fb08855eefd507c
filiploskovski <filiploskovski11@gmail.com>
2020-12-17 11:12:40 +0100

    Add project files.

---
+++ BestFixedMatches1x2/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestFixedMatches1x2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Results()
+        {
+            return View();
+        }
+
+        public IActionResult BettingOdds()
+        {
+            return View();
+        }
+
+        public IActionResult VipTicketArchive()
+        {
+            return View();
+        }
+        public IActionResult VipTicketArchiveSingle(int id)
+        {
+            return View();
+        }
+
+        public IActionResult MonthlySubscriptionArchive()
+        {
+            return View();
+        }
+        public IActionResult FreeTipsArchive()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Advertise()
+        {
+            return View();
+        }
+
+        public IActionResult ToS()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ BestFixedMatches1x2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BestFixedMatches1x2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
