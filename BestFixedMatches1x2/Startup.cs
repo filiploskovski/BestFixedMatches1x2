01c127e530b6fff5ccd65f3f9c7f758d480f0b4e
filiploskovski <filiploskovski11@gmail.com>
2021-01-03 02:29:00 +0100

    1.0.12 auth baner

--- BestFixedMatches1x2/Startup.cs
+++ BestFixedMatches1x2/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,10 +26,10 @@ namespace BestFixedMatches1x2
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, config =>
-            {
-                config.Cookie.Name = "LoginCookie";
-                config.LoginPath = "/Admin";
-            });
+                {
+                    config.Cookie.Name = "LoginCookie";
+                    config.LoginPath = "/Admin";
+                });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
